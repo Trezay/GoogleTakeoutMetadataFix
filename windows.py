0d79cedf007d55b28ca801cf6309f32e77199d93
Biemans <59511634+Trezay@users.noreply.github.com>
2025-09-21 14:08:59 +0200

    .supplemental-metadata.json and modified fix

--- windows.py
+++ windows.py
@@ -5,17 +5,31 @@ from datetime import datetime, timezone
 import win32file
 import win32con
 
-def modify_time(file_path, timestamp):
-    dt = datetime.fromtimestamp(timestamp, tz=timezone.utc)
+def modify_time(file_path, timestampcreated, timestampmodified):
+    dt = datetime.fromtimestamp(timestampcreated, tz=timezone.utc)
+    dtc = datetime.fromtimestamp(timestampmodified, tz=timezone.utc)
     handle = win32file.CreateFile(file_path, win32file.GENERIC_WRITE, 0, None, win32con.OPEN_EXISTING, win32con.FILE_ATTRIBUTE_NORMAL, None)
-    win32file.SetFileTime(handle, dt, None, None)
+    win32file.SetFileTime(handle, dt, None, dtc)
     handle.close()
 
-def get_timestamp(data):
+def get_timestampcreated(data):
     if 'photoTakenTime' in data and 'timestamp' in data['photoTakenTime']:
+        print("pieter1") #debug
         return int(data['photoTakenTime']['timestamp'])
     elif 'created' in data:
+        print("post1") #debug
         dt = datetime.strptime(data['created'], "%Y-%m-%dT%H:%M:%S.%fZ")
+        return int(dt.timestamp())
+    else:
+        raise KeyError("No valid timestamp found in JSON data")
+
+def get_timestampmodified(data):
+    if 'creationTime' in data and 'timestamp' in data['creationTime']:
+        print("pieter2") #debug
+        return int(data['creationTime']['timestamp'])
+    elif 'content_last_modified' in data:
+        print("post2") #debug
+        dt = datetime.strptime(data['content_last_modified'], "%Y-%m-%dT%H:%M:%SZ")
         return int(dt.timestamp())
     else:
         raise KeyError("No valid timestamp found in JSON data")
@@ -25,8 +39,10 @@ def process_file(directory, filename):
     json_paths = [
         os.path.join(directory, f"{filename}.json"),
         os.path.join(directory, f"{filename}-info.json"),
+        os.path.join(directory, f"{filename}.supplemental-metadata.json"),
         os.path.join(directory, f"{base_filename}.json"),
-        os.path.join(directory, f"{base_filename}-info.json")
+        os.path.join(directory, f"{base_filename}-info.json"),
+        os.path.join(directory, f"{base_filename}.supplemental-metadata.json")
     ]
     jpg_path = os.path.join(directory, filename)
 
@@ -35,9 +51,10 @@ def process_file(directory, filename):
             try:
                 with open(json_path, 'r') as json_file:
                     data = json.load(json_file)
-                    timestamp = get_timestamp(data)
-                    modify_time(jpg_path, timestamp)
-                    print(f"Updated {jpg_path} with timestamp {timestamp}")
+                    timestampcreated = get_timestampcreated(data)
+                    timestampmodified = get_timestampmodified(data)
+                    modify_time(jpg_path, timestampcreated, timestampmodified)
+                    print(f"Updated {jpg_path} with timestamp {timestampcreated}")
                 
                 os.remove(json_path)
                 print(f"Removed {json_path}")
